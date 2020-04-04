bf5945041a66de088ac715349564544ed1db25dc
rbriday <ridaya100@gmail.com>
2020-04-04 21:14:25 +0600

    boom

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+$(function(){
+    "use strict";
+
+ $(window).load(function(){
+      $(".items-group").mCustomScrollbar({
+        mouseWheel: true,
+        scrollButtons: {enable:true}
+      });
+    });
+
+});
